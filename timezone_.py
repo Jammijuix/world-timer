bae8796a3a3e2559a32e2d6104f6a6f8d3f0852c
Nnamdi Kelvin Etumnu <etumzykelvin@gmail.com>
2023-08-04 12:54:52 +0100

    add timezone of cities

--- timezone_.py
+++ timezone_.py
@@ -3,14 +3,15 @@ import pycountry
 import pycountry_convert as Pc
 from datetime import datetime
 from datetime import datetime
-x = "Nigeria"
+x = "Canada"
 country  = pycountry.countries
 country_alpha2_code = pycountry.countries.get(name=x)
 w = country_alpha2_code.alpha_2
 print(w)
-#time_zone = pytz.all_timezones #timezone("Africa/Lagos")
-#tmZone = pytz.country_timezones[w]
-#print(tmZone)
+
+time_zone = pytz.all_timezones #timezone("Africa/Lagos")
+tmZone = pytz.country_timezones[w]
+print(tmZone)
 
 #print(time_zone,"\n")
 '''x = pytz.utc
